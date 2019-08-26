85644cc49f135ac0ff891e6542ce9a6c92375484
Patrick Lin <patlin@amazon.com>
2019-08-26 14:05:01 -0700

    Initial commit for supporting a legacy pipeline + Upgrade to scikit-learn 0.21.3.
    Updated sagemaker-containers dependency to >=2.5.5

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 from glob import glob
 from os.path import basename
 from os.path import splitext
+import sys
 
 from setuptools import setup, find_packages
 
@@ -10,33 +11,66 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+version = os.environ.get('SKLEARN_CONTAINER_VERSION')
 
-setup(
-    name='sagemaker_sklearn_container',
-    version='1.0',
-    description='Open source library for creating SciKit-Learn containers to run on Amazon SageMaker.',
+if (version is None or version == 'release'):
+    setup(
+        name='sagemaker_sklearn_container',
+        version='2.0',
+        description='Open source library for creating SciKit-Learn containers to run on Amazon SageMaker.',
 
-    packages=find_packages(where='src', exclude=('test',)),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+        packages=find_packages(where='src', exclude=('test',)),
+        package_dir={'': 'src'},
+        py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
 
-    long_description=read('README.rst'),
-    author='Amazon Web Services',
-    license='Apache License 2.0',
+        long_description=read('README.rst'),
+        author='Amazon Web Services',
+        license='Apache License 2.0',
 
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-    ],
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            'Programming Language :: Python :: 3.5',
+        ],
 
-    install_requires=['numpy', 'pandas', 'retrying==1.3.3', 'sagemaker-containers >= 2.2.0', 'scikit-learn>=0.20.0', 'six'],
-    extras_require={
-        'test': ['tox', 'flake8', 'coverage', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock', 'Flask', 'boto3>=1.4.8',
-                 'docker-compose', 'sagemaker>=1.3.0', 'PyYAML', 'requests==2.18.4']
-    },
-)
+        install_requires=['numpy', 'pandas', 'retrying==1.3.3', 'sagemaker-containers >= 2.5.5', 'scikit-learn>=0.21.3', 'six'],
+        extras_require={
+            'test': ['tox', 'flake8', 'coverage', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock', 'Flask', 'boto3>=1.4.8',
+                     'docker-compose', 'sagemaker>=1.3.0', 'PyYAML', 'requests==2.18.4']
+        },
+    )
+elif (version == 'legacy'):
+    setup(
+        name='sagemaker_sklearn_container',
+        version='1.0',
+        description='Open source library for creating SciKit-Learn containers to run on Amazon SageMaker.',
+
+        packages=find_packages(where='src', exclude=('test',)),
+        package_dir={'': 'src'},
+        py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+
+        long_description=read('README.rst'),
+        author='Amazon Web Services',
+        license='Apache License 2.0',
+
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.5',
+        ],
+
+        install_requires=['numpy', 'pandas', 'retrying==1.3.3', 'sagemaker-containers >= 2.5.5', 'scikit-learn>=0.20.0', 'six'],
+        extras_require={
+            'test': ['tox', 'flake8', 'coverage', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock', 'Flask', 'boto3>=1.4.8',
+                     'docker-compose', 'sagemaker>=1.3.0', 'PyYAML', 'requests==2.18.4']
+        },
+    )
+else:
+    sys.exit("Customer Error: SKLEARN_CONTAINER_VERSION should only be set to 'release' or 'legacy'")
