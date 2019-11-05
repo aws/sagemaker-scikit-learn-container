dbab04a972df83fbfa3c89cb1fef5a04e29135ae
Wilton Wu <wiltonwu@berkeley.edu>
2019-11-05 15:01:00 -0500

    change: use sagemaker-containers' ProcessRunner for executing entry point

--- test/unit/test_training.py
+++ test/unit/test_training.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
 # may not use this file except in compliance with the License. A copy of
@@ -14,16 +14,21 @@ from __future__ import absolute_import
 
 from mock import MagicMock, patch
 
+import sagemaker_containers.beta.framework as framework
 from sagemaker_sklearn_container import training
 
 
-def mock_training_env(current_host='algo-1', module_dir='s3://my/script', module_name='svm', **kwargs):
-    return MagicMock(current_host=current_host, module_dir=module_dir, module_name=module_name, **kwargs)
+def mock_training_env(current_host='algo-1', module_dir='s3://my/script', user_entry_point='svm', **kwargs):
+    return MagicMock(current_host=current_host, module_dir=module_dir, user_entry_point=user_entry_point, **kwargs)
 
 
-@patch('sagemaker_containers.beta.framework.modules.run_module')
-def test_single_machine(run_module):
+@patch('sagemaker_containers.beta.framework.modules.download_and_install')
+@patch('sagemaker_containers.beta.framework.entry_point.run')
+def test_single_machine(run_entry_point, download_and_install):
     env = mock_training_env()
     training.train(env)
 
-    run_module.assert_called_with('s3://my/script', env.to_cmd_args(), env.to_env_vars(), 'svm')
+    download_and_install.assert_called_with(env.module_dir)
+
+    run_entry_point.assert_called_with(env.module_dir, env.user_entry_point, env.to_cmd_args(), env.to_env_vars(),
+                                       runner=framework.runner.ProcessRunnerType)
