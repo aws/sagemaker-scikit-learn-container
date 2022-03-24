4b61c60a95fefb9bc73131a8e366837eefcab9bf
Nikhil Raverkar <nraverka@amazon.com>
2022-03-24 13:57:54 -0400

    upgrading sklearn to 1.0.2

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
     ],
 
     install_requires=read("requirements.txt"),
