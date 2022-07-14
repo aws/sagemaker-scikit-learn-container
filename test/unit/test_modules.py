f776c4655a26d8aa891d18aa1abc742297038501
Nikhil Raverkar <nraverka@amazon.com>
2022-07-14 16:01:28 -0400

    fixing issue# 106

--- test/unit/test_modules.py
+++ test/unit/test_modules.py
@@ -1,4 +1,11 @@
+import pandas as pd
+
 def test_pandas_version():
     import pandas as pd
     major, minor, patch = pd.__version__.split('.')
     assert major == '1'
+
+
+def test_pyarrow_to_parquet_conversion_regression_issue_106(self):
+    df = pd.DataFrame({'x': [1,2]})
+    df.to_parquet('test.parquet', engine='pyarrow')
