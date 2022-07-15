2268f2f069a22404d6759fc7c0263d192eca1f43
Nikhil Raverkar <nraverka@amazon.com>
2022-07-15 10:08:55 -0400

    reverting numpy to 1.19.2, updating boto3 to 1.24.17, fixing docker build issue and adding regression test

--- test/unit/test_modules.py
+++ test/unit/test_modules.py
@@ -1,4 +1,12 @@
+import pandas as pd
+
+
 def test_pandas_version():
     import pandas as pd
     major, minor, patch = pd.__version__.split('.')
     assert major == '1'
+
+
+def test_pyarrow_to_parquet_conversion_regression_issue_106():
+    df = pd.DataFrame({'x': [1, 2]})
+    df.to_parquet('test.parquet', engine='pyarrow')
