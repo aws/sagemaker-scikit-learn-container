fed21a1590323cf345fc839113d011ecab061124
Eli Davidson <elleedee@amazon.com>
2024-10-08 21:13:01 +0000

    try 1

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
     ],
 
     install_requires=read("requirements.txt"),
@@ -45,5 +46,5 @@ setup(
         'console_scripts': 'serve=sagemaker_sklearn_container.serving:serving_entrypoint'
     },
 
-    python_requires='>=3.6',
+    python_requires='>=3.10',
 )
